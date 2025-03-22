896d191b2a396dd58c6a59faa7bd00b187a75fd2
alexdardawill <dardawill@yandex.ru>
2025-03-22 15:44:37 +0200

    chane name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,7 +24,7 @@ onMounted(() => {
   <div class="min-h-screen flex flex-col items-center justify-center px-4">
     <div class="logo-container mb-8">
       <div class="logo-animation">
-        <img src="/myLogo.svg" alt="Insight Logo" class="w-full h-full" />
+        <img src="/1.jpg" alt="Insight Logo" class="w-full h-full" />
       </div>
     </div>
     
